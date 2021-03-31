78570c9ee30f4eb4d9a0e325f173ec9740b1821d
Maksad Annamuradov <46725876+MaksadAnnamuradov@users.noreply.github.com>
2021-03-30 20:38:57 -0600

    User registration, login are working. API is sending token for each user after login

--- JCMFitnessPostgresAPI/Controllers/UserController.cs
+++ JCMFitnessPostgresAPI/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using JCMFitnessPostgresAPI.DataAccess;
 using JCMFitnessPostgresAPI.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,6 +10,7 @@ using System.Threading.Tasks;
 namespace JCMFitnessPostgresAPI.Controllers
 {
     [Microsoft.AspNetCore.Mvc.Route("api/[controller]")]
+    [Authorize]
     [ApiController]
     public class UserController : ControllerBase
     {
