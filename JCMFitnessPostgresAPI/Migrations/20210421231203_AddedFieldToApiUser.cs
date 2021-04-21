6f302f5b7be58507e34e7d3473f08c4b6ad40d80
Maksad Annamuradov <46725876+MaksadAnnamuradov@users.noreply.github.com>
2021-04-21 17:13:21 -0600

    Added some properties to ApiUser

---
+++ JCMFitnessPostgresAPI/Migrations/20210421231203_AddedFieldToApiUser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JCMFitnessPostgresAPI.Migrations
+{
+    public partial class AddedFieldToApiUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAdmin",
+                table: "AspNetUsers",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "JoinedDate",
+                table: "AspNetUsers",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAdmin",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "JoinedDate",
+                table: "AspNetUsers");
+        }
+    }
+}
